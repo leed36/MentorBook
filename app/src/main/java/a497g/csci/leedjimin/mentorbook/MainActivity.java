002c6ba8334d6575ad612028e3e2916a400c747f
baeks2 <wincred>
2018-05-22 16:08:58 -0700

    all files added

--- app/src/main/java/a497g/csci/leedjimin/mentorbook/MainActivity.java
+++ app/src/main/java/a497g/csci/leedjimin/mentorbook/MainActivity.java
@@ -1,8 +1,11 @@
 package a497g.csci.leedjimin.mentorbook;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,16 +13,41 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        BottomNavigationView bottomNav = findViewById(R.id.NavBottom);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
     }
-//
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.nav_bio:
+                            selectedFragment = new BioFragment();
+                            break;
+                        case R.id.nav_chapter:
+                            selectedFragment = new ChapterFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
