40cdf144c94c30ca19ac2728236596d9fbbbc0eb
baeks2 <wincred>
2018-05-30 00:11:48 -0700

    some functions for bio, profile fragments

--- app/src/main/java/a497g/csci/leedjimin/mentorbook/ProfileFragment.java
+++ app/src/main/java/a497g/csci/leedjimin/mentorbook/ProfileFragment.java
@@ -7,11 +7,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileFragment extends Fragment {
+    DatabaseManager DB;
+    Button Done;
+    Button Cancel;
+    EditText N;
+    EditText UN;
+    EditText W;
+    EditText HL;
+    EditText P;
+    EditText A;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        DB = new DatabaseManager(getActivity());
+        Done = (Button) rootView.findViewById(R.id.doneButton);
+        Cancel = (Button) rootView.findViewById(R.id.cancelButton);
+        N = (EditText) rootView.findViewById(R.id.nameInput);
+        UN = (EditText) rootView.findViewById(R.id.userInput);
+        W = (EditText) rootView.findViewById(R.id.websiteInput);
+        HL = (EditText) rootView.findViewById(R.id.headlineInput);
+        P = (EditText) rootView.findViewById(R.id.phoneInput);
+        A = (EditText) rootView.findViewById(R.id.addressInput);
+        addData();
+        return rootView;
+    }
+
+    public void addData(){
+        Done.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //add datas...
+            }
+        });
     }
 }
