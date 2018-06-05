2e60a411e2c9145ea0155fb1d12a0f2294a37015
glasst <trevg8@live.com>
2018-06-05 14:30:17 -0700

    username,pass,email in database successful

--- app/src/main/java/a497g/csci/leedjimin/mentorbook/MenteeSignUp.kt
+++ app/src/main/java/a497g/csci/leedjimin/mentorbook/MenteeSignUp.kt
@@ -12,6 +12,7 @@ import android.text.TextUtils
 import android.content.Intent
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_mentee_signup.*
+import kotlinx.android.synthetic.main.activity_mentor_signup.*
 
 
 /**
@@ -26,61 +27,59 @@ class MenteeSignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mentee_signup)
-
-        val username: String = mentee_username.text.toString()
-        val email: String = mentee_email.text.toString()
-        val password: String = mentee_pass.text.toString()
-        val password2: String = mentee_pass2.text.toString()
         // Set up the sign in form, get variables
-        mentee_signup_button.setOnClickListener { attemptSignUp(username, email, password, password2) }
+        mentee_signup_button.setOnClickListener{attemptSignUp()}
 
     }
 
-    private fun attemptSignUp(username: String, email: String, password: String, password2: String) {
+    private fun attemptSignUp(){
         var check: Boolean = true
+        val username:String = mentee_username.text.toString()
+        val email:String = mentee_username.text.toString()
+        val password:String = mentee_pass.text.toString()
+        val password2:String = mentee_pass2.text.toString()
 
         /** Check password authenticity **/
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if(!TextUtils.isEmpty(password) && !isPasswordValid(password)){
             check = false
             Toast.makeText(this, "Invalid password, please try again", Toast.LENGTH_LONG).show()
         }
 
-        if (!password.equals(password2)) {
+        if(!password.equals(password2)){
             Toast.makeText(this, "Passwords do not equal, please try again", Toast.LENGTH_LONG).show()
             check = false
         }
 
         /** Check Username **/
 
-        if (!isUsernameValid(username)) {
+        if(!isUsernameValid(username)){
             Toast.makeText(this, "Invalid Username, please try again", Toast.LENGTH_LONG).show()
 
         }
 
 
         /** Check for a valid email address **/
-        if (!TextUtils.isEmpty(username)) {
-            Toast.makeText(this, "Please enter email, try again", Toast.LENGTH_LONG).show()
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Please enter username, try again", Toast.LENGTH_LONG).show()
 
             check = false
         }
-        if (isEmailValid(email)) {
+        if (!isEmailValid(email)) {
             Toast.makeText(this, "Please enter valid email, try again", Toast.LENGTH_LONG).show()
             check = false
         }
 
-        if (check == false) {
+        if(check == false){
             resetSignUp()
-        } else {
+        }else{
 
             /*
              *  Registration is complete, save info in database and go to main activity
              */
-            Toast.makeText(this, "Login Sucessful", Toast.LENGTH_LONG).show()
-
             val user = User(username, email, password, 0)
             val db = DatabaseManager(this)
             db.insertWithEmailAndPW(user)
+            Toast.makeText(this, "Login Sucessful for " + user.getUSERNAME() + ", "+ user.getEMAIL() + ", "+ user.getPASSWORD() , Toast.LENGTH_LONG).show()
 
             val insertIntent = Intent(this, MainActivity::class.java)
             this.startActivity(insertIntent)
@@ -101,19 +100,20 @@ class MenteeSignUp : AppCompatActivity() {
         return check
     }
 
-    fun resetSignUp() {
-        mentee_email.setText("")
-        mentee_username.setText("")
-        mentee_pass2.setText("")
-        mentee_pass.setText("")
+    fun resetSignUp(){
+        mentor_email.setText("")
+        mentor_username.setText("")
+        mentor_pass2.setText("")
+        mentor_pass.setText("")
 
     }
 
-    fun isEmailValid(username: String): Boolean {
+    fun isEmailValid(username:String):Boolean{
         return username.contains("@")
     }
 
-    fun isPasswordValid(password: String): Boolean {
+    fun isPasswordValid(password:String):Boolean{
         return true
     }
+
 }
