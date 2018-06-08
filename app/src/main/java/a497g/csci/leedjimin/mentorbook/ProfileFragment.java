eddc886432e1042d0807b12dcf9ac76a0c42b1bb
baeks2 <wincred>
2018-06-08 15:53:02 -0700

    final push

--- app/src/main/java/a497g/csci/leedjimin/mentorbook/ProfileFragment.java
+++ app/src/main/java/a497g/csci/leedjimin/mentorbook/ProfileFragment.java
@@ -9,10 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProfileFragment extends Fragment {
     DatabaseManager DB;
-    User user = new User();
     Button Done;
     Button Cancel;
     EditText N;
@@ -20,13 +20,15 @@ public class ProfileFragment extends Fragment {
     EditText W;
     EditText HL;
     EditText P;
-    String userNam;
+    String userNam = "";
+    User user;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-        userNam = user.getUSERNAME();
         DB = new DatabaseManager(getActivity());
+        userNam = MainActivity.username;
+        user = DB.findUser(userNam);
         Done = (Button) rootView.findViewById(R.id.doneButton);
         Cancel = (Button) rootView.findViewById(R.id.cancelButton);
         N = (EditText) rootView.findViewById(R.id.nameInput);
@@ -36,7 +38,7 @@ public class ProfileFragment extends Fragment {
         P = (EditText) rootView.findViewById(R.id.phoneInput);
 
         N.setText(user.getNAME());
-        UN.setText(MainActivity.username);
+        UN.setText(user.getUSERNAME());
         W.setText(user.getWEBSITE());
         HL.setText(user.getHEADLINE());
         P.setText(user.getPHONE());
@@ -46,7 +48,12 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v){
                 DB.updateProfile(N.toString(), UN.toString(), W.toString(), HL.toString(), P.toString());
-                DB.findUser(userNam);
+                user = DB.findUser(UN.toString());
+                user.setNAME(N.toString());
+                user.setWEBSITE(W.toString());
+                user.setHEADLINE(HL.toString());
+                user.setPHONE(P.toString());
+                Toast.makeText(getContext(), "Update Successful", Toast.LENGTH_LONG).show();
             }
         });
         return rootView;
