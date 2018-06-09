d39776d18a54c423f44cf067bb184c7bd258eaad
leed36 <daniel_lee17@hotmail.com>
2018-06-08 20:43:01 -0700

    fixed the bio fragments editTexts

--- app/src/main/java/a497g/csci/leedjimin/mentorbook/HomeFragment.java
+++ app/src/main/java/a497g/csci/leedjimin/mentorbook/HomeFragment.java
@@ -1,31 +1,54 @@
 package a497g.csci.leedjimin.mentorbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 
-public class HomeFragment extends Fragment {
+
+public class HomeFragment extends Fragment implements AdapterView.OnItemClickListener{
     @Nullable
 
     DatabaseManager DB;
     EditText searchEdit;
-    ListView listView;
+    ListView listview;
+    ArrayList<User> userList;
+    ArrayAdapter<String> listAdapter;
 //    String userName = MainActivity.username;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         DB = new DatabaseManager(getActivity());
+        userList = DB.searchMentors();
 
         searchEdit = (EditText)rootView.findViewById(R.id.searchEdit);
+        listview = (ListView) rootView.findViewById(R.id.listViewSearch);
+        listAdapter = new ArrayAdapter(this, R.layout.simplerow, userList);
+        listview.setOnItemClickListener(this);
         return rootView;
     }
 
+    public void updateView() {
+
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+        // Then you start a new Activity via Intent
+        Intent intent = new Intent(getActivity(), ViewUserActivity.class);
+        intent.putExtra("username", username);
+        getActivity().startActivity(intent);
+    }
 
 }
---
+++ app/src/main/java/a497g/csci/leedjimin/mentorbook/ViewUserActivity.java
@@ -0,0 +1,6 @@
+package a497g.csci.leedjimin.mentorbook;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewUserActivity extends AppCompatActivity {
+}
